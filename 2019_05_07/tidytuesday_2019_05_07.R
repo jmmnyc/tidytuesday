97687cf71ac0ad9e1410c7cf7985738a878bb380
Jose M. Maldonado <jmmnyc@gmail.com>
2019-05-07 16:51:30 -0400

    Add files via upload

--- 2019_05_07/tidytuesday_2019_05_07.R
+++ 2019_05_07/tidytuesday_2019_05_07.R
@@ -9,9 +9,13 @@ library(Cairo)
 
 student_ratio <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-07/student_teacher_ratio.csv")
 
+# Set custom theme
 my_font <- "Lucida Sans"
+
 base_color <- "#f5f3dc"
+
 font_color <- "#42ac96" # or #213549
+
 my_theme <- theme(text = element_text(family = my_font, color = font_color),
                   rect = element_rect(fill = base_color, color = NA), 
                   plot.background = element_rect(fill = base_color, color = NA), 
@@ -26,38 +30,25 @@ my_theme <- theme(text = element_text(family = my_font, color = font_color),
 
 theme_set(theme_light() + my_theme)
 
-
-
+# Subset data
 plot_data <- student_ratio %>% 
-  filter(country %in% c("Dominican Republic", "Cuba", "Puerto Rico", "Jamaica", "Guyana", 
-                        "Belize", "Barbados", "Grenada", "Dominica", "Saint Lucia",
-                        "Saint Vincent and the Grenadines","Bermuda", "Bahamas", "Antigua and Barbuda",
-                        "Saint Kitts and Nevis", "Sint Maarten (Dutch part)", "Turks and Caicos Islands",
-                        "British Virgin Islands", "Latin America and the Caribbean", "United States of America"),
-         indicator %in% c("Primary Education", "Secondary Education", "Tertiary Education"),
-         year == "2015") %>% 
-  select(country, year, indicator, student_ratio) 
-
-
-plot_data_2 <- student_ratio %>% 
   filter(str_detect(country, ".countries"),
          indicator %in% c("Primary Education", "Secondary Education", "Tertiary Education"),
          year == "2017") %>% 
   select(country, year, indicator, student_ratio)
 
-
-
-
-plot_data_2 %>% 
+# Viz
+plot_data %>% 
   ggplot(aes(student_ratio, reorder(country, -student_ratio), fill = indicator)) +
   geom_point(shape = 21, size = 4, alpha = .6) +
   labs(title = "Less teachers per students in lower income countries in 2017",
-       subtitle = "the impact is bigger in primary schools (elementary)",
+       subtitle = "the difference is greater in primary schools (elementary)",
        x = "Student:Teacher ratio (# of students per teacher)",
        y = "",
        caption = "Source: UNESCO Institute of Statistics \nVisualization by Jose M @Joseph_Mike",
        fill = "") +
   theme(legend.position = "top") +
   scale_fill_manual(values = c("#984EA3", "#FF7F00")) #, "#4DAF4A"))"#984EA3"
+
 
 ggsave("TidyTuesday_2019_05_07.png", device = "png", type = "cairo")
