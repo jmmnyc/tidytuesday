876231f07f50a0f47b78c8a4af3df9c680a5da76
Jose M. Maldonado <jmmnyc@gmail.com>
2019-06-10 16:20:06 -0400

    Add files via upload

--- 2019_06_04/tidytuesday_2019_06_04.R
+++ 2019_06_04/tidytuesday_2019_06_04.R
@@ -25,29 +25,31 @@ my_theme <- theme(text = element_text(family = my_font, color = font_color, face
                   legend.background = element_rect(fill = base_color, color = NA),
                   legend.key = element_rect(fill = base_color),
                   plot.caption = element_text(size = 8, color = "#293042"),
-                  plot.subtitle = element_text(size = 8),
-                  axis.title.x = element_text(margin = margin(20,0,0,0)),
-                  axis.title.y = element_text(margin = margin(0,20,0,0)),
-                  plot.title = element_text(margin = margin(0,0,20,0), size = 14))
+                  plot.subtitle = element_text(size = 10),
+                  axis.title.x = element_text(margin = margin(20,0,0,0), size = 14),
+                  axis.title.y = element_text(margin = margin(0,20,0,0), size = 14),
+                  axis.text.y = element_text(size = 12),
+                  plot.title = element_text(margin = margin(0,0,20,0), size = 18))
 
 theme_set(theme_light() + my_theme)
 
-
+# find countries with over 100 ramen dishes rated
 top_countries <- ramen_ratings %>% 
   group_by(country) %>% 
   tally(sort = TRUE) %>% 
   filter(n >= 100, 
          !is.na(country)) %>% 
-  ungroup() 
+  ungroup()
 
+# filter data for the top 11 countires
 plot_data <- inner_join(ramen_ratings, top_countries, by = "country") %>% select(-n)
 
-test_plot_data <- plot_data %>% mutate(avg.)
-
+# import image of Naruto eating ramen
 img <- png::readPNG("ramen.png")
 
 rast <- grid::rasterGrob(img, interpolate = T)
 
+#create plot
 plot_data %>% 
   ggplot(aes(stars, reorder(country, stars, median))) +
   annotation_custom(rast, ymin = 1, ymax = 6, xmin = 5.5) +
@@ -60,9 +62,11 @@ plot_data %>%
   scale_fill_igv() +
   scale_x_continuous(breaks = seq(0,6,1), limits = c(1,6)) +
   coord_cartesian(clip = "off") +
+  theme(panel.grid.minor.x = element_blank()) +
   labs(title = "Ramen rating distribution by country",
        subtitle = "Countries with over 100 ramen dishes reviewed",
        x = "Rating",
        y = "Country",
        caption = "Data from The Ramen Rater \nVisualization by Jose M @Joseph_Mike") 
 
+ggsave("TidyTuesday_2019_06_04.png", width = 10, height = 6.5,device = "png", type = "cairo")
